e3ddbe4eb2498bb7bb91279ddea569b1fc953df6
Aditey1908 <aditeyrk19@gmail.com>
2025-09-27 15:23:26 +0530

    Initial frontend setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig, type ConfigEnv } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }: ConfigEnv) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
